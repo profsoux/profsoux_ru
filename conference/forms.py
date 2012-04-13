ded30a0ff7964cae5bfd0b21ea03e21b850c87dd
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-13 23:09:59 +0400

    Create GH_PAGES

--- conference/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-from django import forms
-
-from conference.models import Participant
-
-
-class ParticipantForm(forms.ModelForm):
-    class Meta:
-        model = Participant
