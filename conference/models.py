132f4e611fb95329a7ea534eda5a084f2cb71b37
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-16 01:27:18 +0400

    Подсветка активного пункта меню

--- conference/models.py
+++ conference/models.py
@@ -7,6 +7,19 @@ PARTNER_TYPE_CHOISES = (
     )
 
 
+class Menu(models.Model):
+    name = models.CharField("Имя", max_length=64)
+    link = models.CharField("Ссылка", max_length=64)
+    weight = models.IntegerField('Порядок вывода')
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.name, self.link)
+
+
 class Person(models.Model):
     first_name = models.CharField("Имя", max_length=64)
     last_name = models.CharField("Фамилия", max_length=64)
