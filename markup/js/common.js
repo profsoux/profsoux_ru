8b19af1ab56bd1b22944c4b8a48ace4de771aea1
unknown <Stas.Kurilov@unit-253.Labs.IntelliJ.Net>
2012-04-18 16:05:09 +0400

    - Пофиксил неверные высоты для событий в расписании; - Добавил тултипы для событий в расписании; - Рефакторинг js;

--- markup/js/common.js
+++ markup/js/common.js
@@ -1,47 +1,87 @@
 var ui = {};
-ui.confDate = new Date(2012, 5-1, 19);
+ui.CONF_DATE = new Date(2012, 5-1, 19);
 ui.schedule = {
+    $schedule: null,
+
     init: function() {
         var $schedule = $("#schedule"),
-            $currentTimeEl, $timeLabels,
+            markerPlacementDate = new Date();
+
+        /*
+        markerPlacementDate = new Date(
+            ui.CONF_DATE.getFullYear(),
+            ui.CONF_DATE.getMonth(),
+            ui.CONF_DATE.getDate(), 12, 15);
+        */
+
+        if ($schedule.length == 0) {
+            return false;
+        }
+
+        this.$schedule = $schedule;
+        this.initTooltips();
+
+        if (ui.CONF_DATE.getFullYear() == markerPlacementDate.getFullYear() &&
+            ui.CONF_DATE.getMonth() == markerPlacementDate.getMonth() &&
+            ui.CONF_DATE.getDate() == markerPlacementDate.getDate()) {
+            this.initTimeMarker(markerPlacementDate);
+        }
+    },
+
+    initTooltips: function() {
+        var $events = this.$schedule.find('.event');
+        $events.each(function() {
+            var eventLength = this.className.match(/div\-([0-9]{1,2})/),
+                tooltipText = '';
+
+            if (eventLength != null) {
+                eventLength = Number(eventLength[1]);
+                if (eventLength == 60) {
+                    tooltipText = 'Длится 1 час';
+                } else if (eventLength < 60) {
+                    tooltipText = 'Длится '+ eventLength +' минут';
+                }
+
+                this.setAttribute('rel', 'tooltip');
+                this.setAttribute('data-original-title', tooltipText);
+                $(this).tooltip({
+                    animation: false,
+                    placement: 'left'
+                });
+            }
+        });
+    },
+
+    initTimeMarker: function(placementDate) {
+        var $schedule = this.$schedule,
+            $timeElement, $timeLabels,
             labelHeight,
-            //now = new Date(2012, 4-1, 14, 12, 45),
-            now = new Date(),
-            confDate = ui.confDate,
-            currentHours,
-            currentMinutes,
+            placementDate = (placementDate instanceof Date) ? placementDate : new Date(),
+            placementHours,
+            placementMinutes,
             parseHourRegexp,
             startHour,
             endHour,
             inHourPixelVal, timeTopMargin;
 
-        if ( $schedule.length == 0) {
-            return;
-        }
-        if (confDate.getFullYear() != now.getFullYear() ||
-            confDate.getMonth() != now.getMonth() ||
-            confDate.getDate() != now.getDate) {
-            // comment this return for debug
-            return false;
-        }
-
-        currentHours = now.getHours();
-        currentMinutes = now.getMinutes();
-
-        $currentTimeEl = $schedule.find(".current-time");
+        // init vars block
+        placementHours = placementDate.getHours();
+        placementMinutes = placementDate.getMinutes();
+        $timeElement = $schedule.find(".current-time");
         $timeLabels = $schedule.find(".caption");
         parseHourRegexp = /time_([0-9]{1,2})\-[0-9]{1,2}/;
         labelHeight = $timeLabels[0].offsetHeight;
         startHour = $timeLabels[0].className.match(parseHourRegexp);
         endHour = $timeLabels[$timeLabels.length-1].className.match(parseHourRegexp);
-        inHourPixelVal = Math.round((currentMinutes / 60) * labelHeight);
+        inHourPixelVal = Math.round((placementMinutes / 60) * labelHeight);
 
         if (startHour != null && endHour != null) {
             startHour = Number(startHour[1]);
             endHour = Number(endHour[1]);
-            timeTopMargin = ((currentHours - startHour) * labelHeight) + (inHourPixelVal);
+            timeTopMargin = ((placementHours - startHour) * labelHeight) + (inHourPixelVal);
+            timeTopMargin -= 1;
 
-            $currentTimeEl.css({
+            $timeElement.css({
                 'display': 'block',
                 'top': timeTopMargin
             });
