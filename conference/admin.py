755d3baa35885ef77a1a121abe56fc0304bcde6f
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-05-18 09:33:36 +0400

    Новые рассылки

--- conference/admin.py
+++ conference/admin.py
@@ -17,32 +17,26 @@ class ParticipantAdmin(admin.ModelAdmin):
 
         emails = []
 
-        subject = u'Осталось 4 дня до конференции ПрофсоUX (СПб, 19 мая 2012, суббота)'
+        subject = u'Конференция ПрофсоUX // 19 мая (суббота), начало регистрации в 11:30'
         sender = u'Оргкомитет конференции ПрофсоUX <contact@profsoux.ru>'
         reply = 'no-reply@profsoux.ru'
         text = u"""Здравствуйте, %s!
 
-Хочу уточнить, придете ли вы на конференцию ПрофсоЮкс 19 мая, в субботу, 12.00, НИУ ИТМО (http://profsoux.ru/location).
-Нам хотелось бы понять, сколько нужно программок, сувениров и кофе с печеньками.
+мы ждем вас на конференции ПрофсоUX завтра, 19 мая (суббота).
+11:30 - начало регистрации и приветственный кофе с печеньками.
 
-Если вы придете, нажмите "Я пойду" вот здесь:
-http://profsoux.ru/registration/confirm/?id=%s&code=%s&action=yes
+Место проведения:
+ИТМО, Кронверкский пр., 49, вход с ул. Сытнинской (www.profsoux.ru/location)
+Ст. метро Горьковская.
 
-Если не сможете прийти, нажмите "Не пойду" здесь:
-http://profsoux.ru/registration/confirm/?id=%s&code=%s&action=no
-(У вас будет шанс посмотреть онлайн трансляцию конференции на www.ProfsoUX.ru).
+Программа: www.profsoux.ru/schedule
+Оnline-трансляция будет доступна бесплатно всем желающим на сайте www.profsoux.ru.
 
-
-Если у вас есть вопросы или вы не доверяете длинным ссылкам, пишите, звоните, мои контакты ниже.
+Вопросы присылайте, пожалуйста, в twitter #profsoux.
 
 С уважением,
-
-Юлия Крючкова,
-Председатель оргкомитета www.ProfsoUX.ru
-
-Email: contact@ux-spb.ru
-Тел. офис: +7 (812) 336 93 44, моб.: +7 (921) 741 48 23
-Skype: julvk70
+Юлия Крючкова
+www.profsoUX.ru
 """
         for participant in queryset:
             m = md5()
@@ -69,7 +63,59 @@ Skype: julvk70
 
         connection.close()
 
-    send_mail.short_description = u"Разослать письмо с подтверждением"
+    send_mail.short_description = u"Разослать письмо О дате и месте"
+
+    def send_another_mail(self, request, queryset):
+        from django.core import mail
+
+        emails = []
+
+        subject = u'Online-трансляция конференции «ПрофсоUX» (www.profsoux.ru) // 19 мая (суббота) 12:30'
+        sender = u'Оргкомитет конференции ПрофсоUX <contact@profsoux.ru>'
+        reply = 'no-reply@profsoux.ru'
+        text = u"""Здравствуйте, %s!
+
+В рамках первой петербургской конференции по usability и user experience
+на сайте www.profsoux.ru организована online-трансляция докладов и панельных дискуссий.
+
+Доступ к трансляции бесплатен. Приглашаются все желающие.
+Начало трансляции: 12:30, 19 мая 2012 года (суббота).
+
+После окончания конференции на сайте будут опубликованы видеозаписи докладов.
+Доступ к ним также будет бесплатен.
+
+Вопросы по online-трансляции, как и в целом по конференции, присылайте, пожалуйста, в twitter (#profsoux).
+
+С уважением,
+Оргкомитет конференции ПрофсоUX
+www.profsoUX.ru
+"""
+        for participant in queryset:
+            m = md5()
+            m.update(participant.email)
+            code = m.hexdigest()
+            recipients = [participant.email]
+
+            message = text % (participant.first_name,
+                participant.id,
+                code,
+                participant.id,
+                code
+                )
+
+            emails.append(
+                mail.EmailMessage(subject, message, sender, recipients,
+                    headers={'Reply-To': reply})
+                )
+
+        connection = mail.get_connection()
+        connection.open()
+
+        connection.send_messages(emails)
+
+        connection.close()
+
+    send_mail.short_description = u"Разослать письмо о трансляции"
 
 
 class PartnerAdmin(admin.ModelAdmin):
