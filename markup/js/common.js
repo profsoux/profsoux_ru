1528e7db67258d94719f168c2478f138311d0041
Stas Kurilov <qtuzov@gmail.com>
2012-05-18 15:38:55 +0400

    Вывод Twitter-ленты на главной, pre-alpha :) (#50)

--- markup/js/common.js
+++ markup/js/common.js
@@ -100,63 +100,262 @@ ui.schedule = {
 /**
  * Tweets stream
   */
-ui.tweetsStream = {
+ui.twee = {
+    // tweets list
+    $list: null,
 
-    init: function(options) {
-        var that = this;
+    // service time interval
+    __t: null,
 
-        that.renderTweets(options.searchQuery);
+    // receiving data in progress flag
+    isReceivingData: false,
+
+    // data updated flag
+    isDataUpdated: false,
+
+    // data update interval
+    dataUpdateInterval: 10000,
+
+    // tweets list update interval
+    listUpdateInterval: 3000,
+
+    // animation duration
+    updateAnimationDuration: 700,
+
+    // tweets storage
+    tweets: [],
+
+    // visible tweets quantity
+    tweetsToShow: 3,
+
+    // current right tweet position
+    currentPosition: 0,
+
+    // test node
+    testNode: null,
+
+    // orientation
+    orientation: 'x',
+
+    queryParams: {
+        rpp: 20,
+        result_type: 'recent',
+        since_id: 0
     },
 
-    renderTweets: function(q) {
-        var list = $("#confTweets"),
-            tweets = [],
-            url = 'http://search.twitter.com/search.json?q=' + escape(q) + '&rpp=20&callback=?';
+    // current state
+    state: 'paused',
 
-        if (list.length == 0) {
+    // tweet text replacement pattern
+    tweetTextReplacePattern: /(^|\s)(?:#([a-zA-Zа-яА-Я0-9_]+)|@([\d\w_]{1,15}))|(https?:\/\/[^\s"]+[\d\w_\-\/])|([^\s:@"]+@[^\s:@"]*)/gi,
+
+    init: function(options) {
+        var that = this,
+            testNode;
+
+        that.$list = $('#confTweets');
+        if (that.$list.length == 0) {
             return false;
         }
 
-        $.getJSON( url, function( data ) {
-            $.each( data.results, function( i, item )
+        // create test node
+        testNode = document.createElement('ul');
+        testNode.className = 'twee-test-node';
+        document.body.appendChild(testNode);
+        that.testNode = testNode;
+
+        // initial tweets render
+        that.getTweets(options.searchQuery);
+        // observe for updates
+        that.__t = setInterval(function()
+        {
+            var from = that.tweetsToShow - 1,
+                to = 0;
+
+            if (that.isDataUpdated === true) {
+                that.renderTweets(from, to);
+                that.currentPosition = from;
+                that.isDataUpdated = false;
+                clearInterval(that.__t);
+            }
+        }, 50);
+
+        if (that.state == 'paused') {
+            that.state = 'normal';
+        }
+        // set roll interval
+        setInterval(function()
+        {
+            that.spinUp();
+        }, that.listUpdateInterval);
+
+        // set data update
+    },
+
+    getTweets: function(q) {
+        var that = this,
+            tweets = [],
+            url = 'http://search.twitter.com/search.json?q='+ escape(q) + '&callback=?';
+
+        that.isReceivingData = true;
+        $.getJSON(url, that.queryParams, function(data) {
+            var reverseTweets = [],
+                i, length;
+
+            if (data.results.length > 0)
             {
-                var tweet = {},
-                    now = new Date(),
-                    user = item.from_user,
-                    image = item.profile_image_url,
-                    text = item.text,
-                    date = null,
-                    str = '',
-                    created_at = Date.parse(item.created_at);
+                $.each(data.results, function(i, item)
+                {
+                    var tweet = {};
+                    //if (item.text.indexOf("RT ") != 0) {
+                    tweet.id = item.id;
+                    tweet.user = item.from_user;
+                    tweet.avatar = item.profile_image_url;
+                    tweet.text = item.text;
+                    tweet.created_at = Date.parse(item.created_at);
+                    tweets.push(tweet);
+                });
+            }
 
-                text = text.replace(
-                    /(^|\s)(?:#([\d\w_]+)|@([\d\w_]{1,15}))|(https?:\/\/[^\s"]+[\d\w_\-\/])|([^\s:@"]+@[^\s:@"]*)/gi,
-                    function( all, space, hashtag, username, link, email ) {
-                        var res = '<a href="mailto:' + email + '">' + email + "</a>";
-                        hashtag && (res = space + '<a href="http://search.twitter.com/search?q=%23' + hashtag + '">#' + hashtag + "</a>");
-                        username && (res = space + '<a href="http://twitter.com/' + username + '">@' + username + "</a>");
-                        link && (res = '<a href="' + encodeURI(decodeURI(link.replace(/<[^>]*>/g, ""))) + '">' + link + "</a>");
-                        return res;
-                    }
-                );
+            // get new tweets from this id
+            if (data.max_id_str) {
+                that.queryParams.since_id = data.max_id_str;
 
-                tweet.user = user;
-                tweet.image = image;
-                tweet.text = text;
+                // debug id
+                //that.queryParams.since_id = 203374752235139070;
+            }
 
-                if (created_at != NaN) {
-                    tweet.date = now.setTime(created_at);
+            // initial call
+            if (that.tweets.length == 0) {
+                // reverse array
+                i = tweets.length;
+                while (i > 0) {
+                    i--;
+                    reverseTweets.push(tweets[i]);
                 }
-
-                tweets.push('<li class="span4"><a href="http://twitter.com/' +
-                    user + '" title="@' +
-                    user + '"><img src="' +
-                    image + '"></a><p>' +
-                    text + '</p></li>');
-            });
-            list.append(tweets.join(''));
+                that.tweets = reverseTweets;
+            } else {
+                if (tweets.length > 0) {
+                    i = tweets.length;
+                    while (i > 0) {
+                        i--;
+                        that.tweets.push(tweets[i]);
+                    }
+                }
+            }
+            that.isDataUpdated = true;
+            that.isReceivingData = false;
         });
-        return tweets;
+    },
+
+    renderTweet: function(tweet) {
+        var that = this,
+            tweetTextReplacePattern = that.tweetTextReplacePattern,
+            tweetTextFormatted, tweetBody;
+
+        tweetTextFormatted = tweet.text.replace(
+            tweetTextReplacePattern,
+            function(all, space, hashtag, username, link, email) {
+                var res = '<a href="mailto:' + email + '">' + email + "</a>";
+                hashtag && (res = space + '<a href="http://search.twitter.com/search?q=%23' + hashtag + '">#' + hashtag + "</a>");
+                username && (res = space + '<a href="http://twitter.com/' + username + '">@' + username + "</a>");
+                link && (res = '<a href="' + encodeURI(decodeURI(link.replace(/<[^>]*>/g, ""))) + '">' + link + "</a>");
+                return res;
+            }
+        );
+        tweetBody = '<div class="tweet span4"><a href="http://twitter.com/' +
+            tweet.user + '" title="@' +
+            tweet.user + '"><img src="' +
+            tweet.avatar + '"></a><p>' +
+            '<a href="http://twitter.com/'+ tweet.user +'" class="twitter-user">'+ tweet.user +'</a>&nbsp;' +
+            tweetTextFormatted +'</p></div>';
+
+        return tweetBody;
+    },
+
+    renderTweets: function(from, to) {
+        var that = this,
+            $list = that.$list,
+            tweets = that.tweets,
+            from = (typeof from === 'number') ? from : 0,
+            to = (typeof to === 'number') ? to : tweets.length,
+            tweetsFormatted = [],
+            i;
+
+        // reverse iteration
+        if (from > to)
+        {
+            i = from;
+            while (i >= to) {
+                tweetsFormatted.push('<li>' + that.renderTweet(tweets[i]) + '</li>');
+                i--;
+            }
+        }
+        // normal
+        else {
+            for (i = from; i < to; i++) {
+                tweetsFormatted.push('<li>' + that.renderTweet(tweets[i]) + '</li>');
+            }
+        }
+        $list.html(tweetsFormatted.join(''));
+    },
+
+    addTweet: function(tweet) {
+        var that = this,
+            $list = that.$list,
+            tweets = that.tweets,
+            testNode = that.testNode,
+            tweetRendered, $tweetNode,
+            li, liTest,
+            tweetWidth;
+
+        tweetRendered = that.renderTweet(tweet);
+
+        // get tweet width
+        liTest = document.createElement('li');
+        liTest.innerHTML = tweetRendered;
+        testNode.appendChild(liTest);
+        tweetWidth = liTest.offsetWidth;
+        testNode.removeChild(liTest);
+
+        // prepend tweet
+        li = document.createElement('li');
+        li.innerHTML = tweetRendered;
+        li.style.width = '0';
+
+        $tweetNode = $(li).find('.tweet');
+        $tweetNode.css('display', 'none');
+        $list.prepend(li);
+
+        $(li).animate({width: tweetWidth+'px'}, that.updateAnimationDuration, function() {
+            $tweetNode.fadeIn();
+            li.style.width = 'auto';
+        });
+    },
+
+    pause: function() {
+        this.state = 'paused';
+    },
+
+    resume: function() {
+        this.state = 'normal';
+    },
+
+    spinUp: function() {
+        if (this.state == 'paused') {
+            return false;
+        }
+
+        var that = this,
+            $list = that.$list,
+            tweets = that.tweets,
+            nextTweet, newPosition;
+
+        newPosition = that.currentPosition + 1;
+        if (newPosition < that.tweets.length) {
+            that.addTweet(tweets[newPosition]);
+            that.currentPosition = newPosition;
+        }
     }
 };
 
@@ -165,16 +364,16 @@ ui.tweetsStream = {
  */
 ui.videoStream = {
     playerTemplate:
-        '<iframe width="{width}" height="{height}" src="http://cdn.livestream.com/embed/{stream_profile}?layout=4&amp;height={height}&amp;width={width}&amp;autoplay={autoplay}" style="border:0;outline:0" frameborder="0" scrolling="no"></iframe>' +
+        '<iframe width="{width}" height="{height}" src="http://cdn.livestream.com/embed/{streamProfile}?layout=4&amp;height={height}&amp;width={width}&amp;autoplay={autoplay}" style="border:0;outline:0" frameborder="0" scrolling="no"></iframe>' +
         '<div style="font-size:11px; padding-top:10px; text-align:center;">Watch ' +
         '<a href="http://www.livestream.com/?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="live streaming video">live streaming video</a> from ' +
-        '<a href="http://www.livestream.com/{stream_profile}?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="Watch ProfsoUX at livestream.com">ProfsoUX Conference</a> at livestream.com' +
+        '<a href="http://www.livestream.com/{streamProfile}?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="Watch ProfsoUX at livestream.com">ProfsoUX Conference</a> at livestream.com' +
         '</div>',
 
     $playerContainer: null,
     width: 640,
     height: 385,
-    stream_profile: 'profsoux',
+    streamProfile: 'profsoux',
     autoplay: false,
 
     init: function(options) {
@@ -189,7 +388,8 @@ ui.videoStream = {
             }
         }
 
-        if (that.$playerContainer === null) {
+        that.$playerContainer = $('#confVideoStream');
+        if (that.$playerContainer.length == 0) {
             return false;
         }
 
@@ -213,7 +413,7 @@ ui.videoStream = {
 
         playerStr = playerTemplate.replace(/\{width\}/g, width)
             .replace(/\{height\}/g, height)
-            .replace(/\{stream_profile\}/g, that.stream_profile)
+            .replace(/\{streamProfile\}/g, that.streamProfile)
             .replace(/\{autoplay\}/g, that.autoplay);
 
         that.$playerContainer.html(playerStr);
@@ -222,20 +422,16 @@ ui.videoStream = {
 
 
 $(function(){
-    $playerContainer = $('#confVideoStream');
-
+    // schedule
     ui.schedule.init();
 
     // tweets stream
-    ui.tweetsStream.init({
+    ui.twee.init({
         searchQuery: '#profsoux'
     });
 
-    // video stream init
-    if($playerContainer.length) {
-        ui.videoStream.init({
-            $playerContainer: $('#confVideoStream'),
-            stream_profile: 'alxmkv'
-        });
-    }
+    // video stream
+    ui.videoStream.init({
+        streamProfile: 'profsoux'
+    });
 });
