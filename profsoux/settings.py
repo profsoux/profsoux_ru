6f0c895e603cdeb7f7a3b1b42f6b394a6bb081d7
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-01-18 01:24:09 +0400

    Явное указание формата даты в settings.py
    Close profsoux/profsoux_ru#16

--- profsoux/settings.py
+++ profsoux/settings.py
@@ -46,7 +46,11 @@ USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
-USE_L10N = True
+USE_L10N = False
+
+LANGUAGE_CODE = 'ru-RU'
+
+DATE_FORMAT = 'd E Y'
 
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
