01a94885f90fc9af309596d2a28ed9984a059f6a
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-05-16 23:29:10 +0400

    Заменил код трансляции на главной

--- markup/js/common.js
+++ markup/js/common.js
@@ -1036,67 +1036,6 @@ ui.twee = {
     }
 };
 
-/**
- * Livestream
- */
-ui.videoStream = {
-    playerTemplate:
-        '<iframe width="{width}" height="{height}" src="http://cdn.livestream.com/embed/{streamProfile}?layout=4&amp;height={height}&amp;width={width}&amp;autoplay={autoplay}" style="border:0;outline:0" frameborder="0" scrolling="no"></iframe>' +
-            '<div style="font-size:11px; padding-top:10px; text-align:center;">Watch ' +
-            '<a href="http://www.livestream.com/?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="live streaming video">live streaming video</a> from ' +
-            '<a href="http://www.livestream.com/{streamProfile}?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="Watch ProfsoUX at livestream.com">ProfsoUX Conference</a> at livestream.com' +
-            '</div>',
-
-    $playerContainer: null,
-    width: 640,
-    height: 385,
-    streamProfile: 'profsoux',
-    autoplay: false,
-
-    init: function(options) {
-        var that = this,
-            option;
-
-        if (typeof options === 'object') {
-            for (option in options) {
-                if (option in that) {
-                    that[option] = options[option];
-                }
-            }
-        }
-
-        that.$playerContainer = $('#confVideoStream');
-        if (that.$playerContainer.length == 0) {
-            return false;
-        }
-
-        that.showPreloader();
-        that.renderPlayer();
-    },
-
-    showPreloader: function() {
-        var that = this;
-        that.$playerContainer.html('Loading...');
-    },
-
-    renderPlayer: function() {
-        var that = this,
-            playerTemplate = that.playerTemplate,
-            width, height,
-            playerStr = '';
-
-        width = that.$playerContainer.get(0).offsetWidth;
-        height = Math.round(width * 0.75); // 4/3
-
-        playerStr = playerTemplate.replace(/\{width\}/g, width)
-            .replace(/\{height\}/g, height)
-            .replace(/\{streamProfile\}/g, that.streamProfile)
-            .replace(/\{autoplay\}/g, that.autoplay);
-
-        that.$playerContainer.html(playerStr);
-    }
-};
-
 
 $(function(){
     // schedule
